f7479ab4d71bd5a506aeef8e64dd6e93f19b2dcc
Nicholas Garrett <nick.garrett@fluxfederation.com>
2017-10-19 12:14:21 +1300

    blah

--- app/controllers/midi_controller.rb
+++ app/controllers/midi_controller.rb
@@ -1,10 +1,10 @@
 class MidiController < ApplicationController
 
   def show
-    midi = Midi.new
-    midi.record(5)
-    while true
-      midi.play_recording
-    end
-  end
+  #   midi = Midi.new
+  #   midi.record(5)
+  #   while true
+  #     midi.play_recording
+  #   end
+  # end
 end
